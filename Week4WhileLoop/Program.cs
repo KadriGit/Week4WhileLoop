9c29d329e24ae8dab01eca74507997ab66d40a8a
KadriGit <kadri.viira@tktk.ee>
2024-10-29 23:23:31 +0200

    fourth

--- Week4WhileLoop/Program.cs
+++ Week4WhileLoop/Program.cs
@@ -4,15 +4,18 @@ int cpuRandom = rnd.Next(1, 4);
 
 bool loopactive = true;
 
+int numberOfTries = 0;
+
 while (loopactive)
 {
     Console.WriteLine("Try to guess the cpu number : ");
     int userguess = Convert.ToInt32(Console.ReadLine());
-
+    numberOfTries++;    
     if(userguess == cpuRandom)
     {
         Console.WriteLine("you won !");
         loopactive = false;
     }
 }
+Console.WriteLine($"The number of tries: {numberOfTries}");
 Console.WriteLine("Have a nice days!");
