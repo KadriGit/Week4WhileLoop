7607a42469a6752ae84b92be9039b31b5360e859
KadriGit <kadri.viira@tktk.ee>
2024-10-29 23:15:04 +0200

    second

--- Week4WhileLoop/Program.cs
+++ Week4WhileLoop/Program.cs
@@ -6,4 +6,11 @@ while (true)
 {
     Console.WriteLine("Try to guess the cpu number : ");
     int userguess = Convert.ToInt32(Console.ReadLine());
+
+    if(userguess == cpuRandom)
+    {
+        Console.WriteLine("you won !");
+        break;
+    }
 }
+Console.WriteLine("Have a nice days!");
